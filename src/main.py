43807c3af67102b51160240191f7bd098d2cec78
varun <varun4socials@gmail.com>
2025-01-01 23:26:17 +0530

    Create menu for new feature and get cgpa homepage

--- src/main.py
+++ src/main.py
@@ -1,6 +1,25 @@
 from userlogin import *
 from attendance import *
 
-studentHomePage = getHomePageAttendance()
-studentData = getStudentPercentage(studentHomePage)
-getAttendance(studentData)
+while True:
+    #Display Menu
+    print("#====Welcome To autoTracc!====#\nMenu :\n 1. Caluculate Attendance \n 2. Calculate CGPA \n 3. Exit")
+
+    #Get and validate user option
+    while True:
+        choice = int(input(">"))
+        if choice not in [1,2,3]:
+            print("Invalid option! Try again!")
+        else:
+            break
+
+    #Execute menu option
+    if choice == 1:
+        studentHomePage = getHomePageAttendance()
+        studentData = getStudentPercentage(studentHomePage)
+        getAttendance(studentData)
+    if choice == 2:
+        getHomePageCGPA()
+    else:
+        print("Thank You!")
+        break
--- src/userlogin.py
+++ src/userlogin.py
@@ -1,3 +1,6 @@
+from requests import Session
+from bs4 import BeautifulSoup
+
 def getUserInput():
     #check rollno input
     while True:
@@ -19,8 +22,6 @@ def getUserInput():
 
 
 def getHomePageAttendance():
-    from requests import Session
-    from bs4 import BeautifulSoup
     #Start a session
     login_url = "https://ecampus.psgtech.ac.in/studzone"
     session = Session()
@@ -29,7 +30,7 @@ def getHomePageAttendance():
     login_page = session.get(login_url)
 
     #Extract the html from the page using lxml parser
-    login_soup = BeautifulSoup(login_page.text , 'lxml')
+    login_soup = BeautifulSoup(login_page.text , "lxml")
 
     #Get the dynamic token used for login
     token = login_soup.find("input",{"name":"__RequestVerificationToken"})["value"]
@@ -39,9 +40,9 @@ def getHomePageAttendance():
 
     #Create a payload to POST to the login form
     payload = {
-    "rollno" : rollno,
-    "password" : password,
-    "chkterms" : "on",
+    "rollno"                     : rollno,
+    "password"                   : password,
+    "chkterms"                   : "on",
     "__RequestVerificationToken" : token
     }
 
@@ -53,6 +54,61 @@ def getHomePageAttendance():
     response_soup = BeautifulSoup(response.text , "lxml")
     check = response_soup.find("nav",{"class":"navbar navbar-expand-lg navbar-light"})
     if check:
+        return session
+    else:
+        return None
+
+
+def getHomePageCGPA():
+    #Start a session
+    login_url = "https://ecampus.psgtech.ac.in/studzone2"
+    session = Session()
+
+    #Get the login page
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    login_page = session.get(login_url,headers=headers)
+
+    #Extract the html from the page using lxml parser
+    login_soup = BeautifulSoup(login_page.text , "lxml")
+
+    #Get the user input
+    rollno, password = getUserInput()
+
+    #Get the dynamic tokens used for login
+    # event_target        = login_soup.find("input",{"name" : "__EVENTTARGET"})["value"]
+    # event_argument      = login_soup.find("input",{"name" : "__EVENTARGUMENT"})["value"]
+    # last_focus          = login_soup.find("input",{"name" : "__LASTFOCUS"})["value"]
+    viewstate           = login_soup.find("input",{"name":"__VIEWSTATE"})["value"]
+    viewstate_generator = login_soup.find("input",{"name":"__VIEWSTATEGENERATOR"})["value"]
+    event_validation    = login_soup.find("input",{"name" : "__EVENTVALIDATION"})["value"]
+    abcd3    = login_soup.find("input",{"name" : "abcd3"})["value"]
+
+    #
+    payload = {
+        "__EVENTTARGET"        : "",
+        "__EVENTARGUMENT"      : "",
+        "__LASTFOCUS"          : "",
+        "__VIEWSTATE"          : viewstate,
+        "__VIEWSTATEGENERATOR" : viewstate_generator,
+        "__EVENTVALIDATION"    : event_validation,
+        "rdolst"               : "S",
+        "txtusercheck"         : rollno,
+        "txtpwdcheck"          : password,
+        "abcd3"                : abcd3
+    }
+
+    print(payload)
+
+    #Get the response from POST method
+    response = session.post(login_url, data=payload)
+    print("Code : ",response.status_code)
+    print(response.text)
+    #Check if we have landed on student home page
+    #and the pass the current session for the next function
+    response_soup = BeautifulSoup(response.text , "lxml")
+    check = response_soup.find("table",{"id":"Title1_Menu1"})
+    if check:
+        print(response_soup.title.text)
         return session
     else:
         return None
