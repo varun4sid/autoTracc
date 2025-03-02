8552b50be904c04bac6d41cbdc334fe9565779b3
varun <varun4socials@gmail.com>
2025-03-02 09:25:22 +0530

    Test: Chromium driver

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
 # [autoTracc](https://autotracc.streamlit.app) 
 
-**autoTracc** is a student dashboard app built using the Python framework [**Streamlit**](https://streamlit.io/), for the students of PSG College of Technology,
-to instantly view the following :
+**autoTracc** is a student dashboard app built using the Python framework [**Streamlit**](https://streamlit.io/), for the students of PSG College of Technology, to instantly view the following :
 
 + **Affordable leaves that can be taken to maintain their attendance percentages**
 
 + **GPA and CGPA in every semester**
 
-+ **Tabulated exam schedule** (in [studzone](https://ecampus.psgtech.ac.in/studzone) it looks as if each exam got an ID card)
++ **Tabulated exam schedule**
 
-<p></p>
++ **Internal marks and set target for their end-semester marks**
+
++ **Autofill feedback forms**
+
+<br>
 
 # Privacy
 The app is designed such that, a user's credentials are used to directly access the frontend of their user profile from the college's official studzone sites to fetch the data. The fetched data is only stored and processed in said user's session to view the above mentioned features.
--- src/feedback.py
+++ src/feedback.py
@@ -10,16 +10,9 @@ import streamlit as st
 
 
 def createDriver():
-    #Check for presence of stable chrome version in streamlit runtime
-    CHROME_PATH = "/usr/bin/google-chrome-stable"
-    if not os.path.exists(CHROME_PATH):
-        os.system("wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb")
-        os.system("apt-get install -y ./google-chrome-stable_current_amd64.deb || apt-get install -fy")
-        os.system("rm google-chrome-stable_current_amd64.deb")
-
     #Set up Selenium Chrome options
     options = webdriver.ChromeOptions()
-    options.binary_location = CHROME_PATH
+    options.binary_location = "/usr/bin/chromium"
     options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
@@ -27,7 +20,7 @@ def createDriver():
     options.add_argument("--disable-gpu")
 
     #Start WebDriver
-    service = Service(ChromeDriverManager().install())
+    service = service = Service("/usr/bin/chromedriver")
     driver = webdriver.Chrome(service=service, options=options)
     
     return driver
@@ -51,17 +44,17 @@ def intermediateForm(rollno,password):
     password_field.send_keys(password)
 
     checkbox = browser.find_element(By.ID,"terms")
-    checkbox.click()
+    browser.execute_script("arguments[0].click();",checkbox)
 
     login_button = browser.find_element(By.ID,"btnLogin")
-    login_button.click()
+    browser.execute_script("arguments[0].click();",login_button)
     
     #Get the feedback index page
     browser.get("https://ecampus.psgtech.ac.in/studzone/Feedback/Intermediate")
     feedbacks_index = browser.find_elements(By.CLASS_NAME,"card-body")
     
     #Click the intermediate feedback 
-    feedbacks_index[1].click()
+    browser.execute_script("arguments[0].click();",feedbacks_index[1])
     
     #Get the courses
     courses = browser.find_elements(By.CLASS_NAME,"intermediate-body")
@@ -71,16 +64,16 @@ def intermediateForm(rollno,password):
         courses = browser.find_elements(By.CLASS_NAME,"intermediate-body")
         course_names = browser.find_elements(By.CSS_SELECTOR,"h6.course")
         progress_bar.progress((course)/len(courses),text=course_names[course].text+"...")  
-        courses[course].click()
+        browser.execute_script("arguments[0].click();",courses[course])
         
         clicks = 0
         while clicks < 12:
             try:
                 radio_button = wait.until(EC.element_to_be_clickable((By.XPATH,f"//label[@for='radio-{clicks+1}-1']")))
-                radio_button.click()
+                browser.execute_script("arguments[0].click();",radio_button)
                 clicks += 1
                 next = wait.until(EC.element_to_be_clickable((By.XPATH,"//button[@class='carousel-control-next']")))
-                next.click()
+                browser.execute_script("arguments[0].click();",next)
             except StaleElementReferenceException:
                 continue
             
