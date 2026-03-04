fa6d11cdc8b80b8d1d40ca1584ac617f94207247
varun <varun4socials@gmail.com>
2026-03-04 15:39:35 +0530

    fix: clear old CGPA state after logout

--- pages/dashBoardPage.py
+++ pages/dashBoardPage.py
@@ -47,6 +47,7 @@ def dashBoardFooter():
     #On clicking logout button session state is reset to login page and script is rerun
     if logout_button:
         st.session_state.page = "login_page"
+        st.session_state.cgpa_available = False
         st.rerun()
         
     st.markdown("""<p>Join the <a href="https://github.com/varun4sid/autoTracc/discussions/new/choose">discussions</a>
